b227def26f73a669d2d5e20f331eb66ed6671668
crisyg97 <cristiangaray101@gmail.com>
2022-02-24 11:24:57 -0300

    fix problem with webpack.config.js

--- app-weather/webpack.config.js
+++ app-weather/webpack.config.js
@@ -1,39 +1,47 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const devMode = process.env.NODE_ENV;
+const devMode = process.env.NODE_ENV !== 'production';
 
-//configura todo para que en el build.js este todo cargado como los archivos js, css y html del proyecto
 module.exports = {
+
     entry: [
-        "./src/index.js"
+        '@babel/polyfill',
+        './src/app/index.js'
     ],
     output: {
-        path: __dirname + "/build",
-        filename: "build.js"
+        path: __dirname + '/build',
+        filename: 'bundle.js'
     },
-    mode: "development",
-    modules: {//array de objetos
-        rules: [ 
+    mode: 'development',
+    
+    module: {
+        rules: [
             {
-                test: /\.js/, //todos los objetos javascript seran cargado por el siguiente modulo
+                test: /\.js$/,
                 loader: 'babel-loader'
             },
             {
-                test: /\.css$/, 
-                use: [ //todos los archivos de tipo css seran cargados por el siguiente modulo
-                    'style-loader',
+                test: /\.css$/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
                     'css-loader'
                 ]
             }
         ]
     },
+
+    devServer : {
+        port: 4000
+    },
+
     plugins: [
-        new HtmlWebpackPlugin({//se inicializa porque es una clase, ubico mi archivo html
-            template: './src/index.html'  
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
         }),
-        new MiniCssExtractPlugin({ //ubico mi archivo css
-            filename: "blunde.css" 
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
         })
     ]
-}
+
+};
